8f3ecba40201a9ed160f798bb5a711919899a886
mutexre <alexander.obuschenko@gmail.com>
2018-09-09 19:38:10 +0700

    stb.c

---
+++ stb.c
@@ -0,0 +1,10 @@
+//
+//  Created by mutexre on 11/06/16.
+//  Copyright © 2016 mutexre. All rights reserved.
+//
+
+#define STB_DEFINE
+#include <stb/stb.h>
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include <stb/stb_image_resize.h>
